04ecb51b411196aaeb31637976ceb4b6d55b33e5
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2019-01-23 21:07:14 +0600

    Add files via upload

---
+++ HomeWork2/HomeWork2/Hw2Context.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork2
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Hw2Context : DbContext
+    {
+        public Hw2Context()
+            : base("name=Hw2Context")
+        {
+        }
+        
+        public DbSet<Gruppa> Gruppas { get; set; }
+    }
+}
